ff42108aa51b52794e29beebf2f8836d464ac69d
Gabby Jones <jones.gm90@gmail.com>
2020-02-13 07:36:57 -0500

    setting file structure and seeding data

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Habenero heat', 'true')
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss army', 'false')
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno business', 'true')
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon the old days', 'true')
